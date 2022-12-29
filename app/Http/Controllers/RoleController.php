3d3e597386a3a4b9c1c81f8cc0d6157c7fba7147
Yefri Gonzalez <yefrig00@gmail.com>
2022-12-29 16:48:23 -0600

    Rutas de editar, desactivar y obtener roles terminado

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -27,4 +27,79 @@ class RoleController extends Controller
             'message'=>'rol creado correctamente'
         ],200);
     }
+
+    /**
+     * @param $request recibe la peticion del frontend
+     * A traves de ELOQUENT podemos usar el metodo select y seleccionar el id y nombre del role con la condicion de que el estado
+     * sea 1, es decir este activo      
+     */
+    public function obtenerRoles(Request $request){
+        $roles=Role::select("id","nombre")
+            ->where("estado",1)
+            ->get();
+        return response()->json($roles);    
+    }
+
+    /**
+     * @param $request recibe la peticion del frontend
+     * $validateData valida los campos, es decir requiee que la peticion contenga dos campos uno sea entero y el otro un string
+     * A traves de ELOQUENT podemos usar el metodo find y seleccionar el rol que corresponde el id
+     * 
+     * Al obtener el rol podemos hacer uso de sus variables y asignarle el valor obtenido en el validateData
+     * Con el metodo save() de ELOQUENT se hace referencia al UPDATE de SQL      
+     */
+
+    public function modificarRol(Request $request){
+        $validateData=$request->validate([
+            'id'=>'required|int',
+            'nombre'=>'required|string'
+        ]);
+        $rol=Role::find($validateData['id']);
+        if(isset($rol)){
+            $rol->nombre=$validateData['nombre'];
+            $rol->save();
+            return response()->json([
+                'status'=>true,
+                'message'=>'rol modificado correctamente'
+            ],200);
+        } else{
+            return response()->json([
+                'status'=>false,
+                'message'=>'Dato no encontrado'
+            ],404);
+        }
+    }
+
+    /**
+     * @param $id recibe el id en la peticion GET
+     * A traves de ELOQUENT podemos usar el metodo find y seleccionar el rol que corresponde el id
+     * 
+     * Al obtener el rol podemos hacer uso de sus variables y asignarle el valor 0 al rol
+     * Con el metodo save() de ELOQUENT se hace referencia al UPDATE de SQL      
+     */
+
+    public function desactivarRol(int $id){
+        try{
+            $rol=Role::find($id);
+            if(isset($rol)){
+                $rol->estado=0;
+                $rol->save();
+                return response()->json([
+                    'status'=>true,
+                    'message'=>'rol desactivado correctamente'
+                ],200);
+            } else{
+                return response()->json([
+                    'status'=>false,
+                    'message'=>'ERROR, dato no encontrado'
+                ],404); 
+            }
+        }catch(\Throwable $th){
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+        
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,18 +23,22 @@ use App\Http\Controllers\AsignacionGrupoController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
 Route::post('/login',[UsuarioController::class,'login']);
 Route::post('/registro',[UsuarioController::class,'register']);
 Route::post('/asignarPermiso',[AsignacionPermisoController::class,'asignarPermisoRol']);
 Route::post('/asignarRol',[AsignacionRolController::class,'asignarRolGrupo']);
 Route::post('/asignarGrupo',[AsignacionGrupoController::class,'asignarGrupoUsuario']);
 Route::post('/rol',[RoleController::class,'createRole']);
+Route::get('/roles',[RoleController::class,'obtenerRoles']);
+Route::get('/rol/{id}',[RoleController::class,'desactivarRol']);
+Route::post('/rol/edit',[RoleController::class,'modificarRol']);
 Route::post('/permiso',[PermisoController::class,'createPermiso']);
 Route::post('/grupo',[GrupoController::class,'createGroup']);
 
 
 Route::group(['middleware' => ['auth:sanctum','ability:admin']], function() {
-    //
+    
 });
 
 Route::post('/logout',[UsuarioController::class,'logout'])->middleware('auth:sanctum');
